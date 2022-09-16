3514ba9df4dcb33a4fb78f0f3a3f4a5aafb17181
andremw96 <andre19961@gmail.com>
2022-09-16 15:52:38 +0700

    save image

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -3,6 +3,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({Key? key}) : super(key: key);
@@ -13,6 +16,22 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
+
+  Future<void> _takePicture() async {
+    final imagePicker = ImagePicker();
+    final imageFile = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    setState(() {
+      _storedImage = File(imageFile!.path);
+    });
+    final appDir = await getApplicationDocumentsDirectory();
+    final fileName = basename(imageFile!.path);
+    final savedImage =
+        await File(imageFile.path).copy("${appDir.path}/$fileName");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +61,7 @@ class _ImageInputState extends State<ImageInput> {
         const SizedBox(width: 10),
         Expanded(
           child: FlatButton.icon(
-              onPressed: () {},
+              onPressed: _takePicture,
               icon: const Icon(Icons.camera),
               label: const Text("take picture"),
               textColor: Theme.of(context).primaryColor),
